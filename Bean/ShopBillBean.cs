e049144d9846626f72aeb7e032300ebf19b3148b
suryakala20 <suryakala257r@gmail.com>
2026-02-14 10:01:33 +0530

    Update ShopBillBean.cs

--- Bean/ShopBillBean.cs
+++ Bean/ShopBillBean.cs
@@ -1,1 +1,14 @@
+using System;
+
+namespace Com.Wipro.Shop.Bean
+{
+    public class ShopBillBean
+    {
+        public int BillID { get; set; }
+        public string ItemID { get; set; }
+        public int Quantity { get; set; }
+        public double TotalAmount { get; set; }
+        public DateTime BillDate { get; set; }
+    }
+}
 
